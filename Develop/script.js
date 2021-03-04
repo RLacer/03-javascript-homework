ac6096fa11db872e967b1d5f082231c7aed44f6e
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-03-04 12:58:50 -0700

    mostly done

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,8 @@ var generateBtn = document.querySelector("#generate");
 
 // possible characters
 
-var specChars = ['!', '<', '@', '<', '#', '<', '$'];
-var numChars = ['0', '1', '2', '3', '4', '5', '6'];
+var specChars = ['!','<','@','<','#','<','$'];
+var numChars = ['0','1','2','3','4','5','6'];
 var upChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 ];
 var lowChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
@@ -21,7 +21,7 @@ function getRandom(arr) {
 // // Write password to the #password input
 
 function writePassword() {
-  var password = generatePassword();
+   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
@@ -35,75 +35,100 @@ function generatePassword() {
   var guarChars = [];
 
   // get random function line 14 use to push guarChars
-  if (options.hasSpecChars) {
-    possChars = possChars.concat(specChars);
-    console.log(possChars);
-    guarChars.push(getRandom(specChars));
+if (options.hasSpecChars) {
+  possChars = possChars.concat(specChars);
+  console.log(possChars);
+  guarChars.push(getRandom(specChars));
 
+};
+// Replicate to other option chars
+if (options.hasNum) {
+  possChars = possChars.concat(numChars);
+  console.log(possChars);
+  guarChars.push(getRandom(numChars));
+  console.log(guarChars);
+}
+if (options.hasUpper) {
+  possChars = possChars.concat(upChars);
+  console.log(possChars);
+  guarChars.push(getRandom(upChars));
+  console.log(guarChars);
+}
+if (options.hasLower) {
+  possChars = possChars.concat(lowChars);
+  guarChars.push(getRandom(lowChars));
+  console.log(guarChars);
+}
+// index for var options, var result line 30, push var newChar
+for (let i = 0; i < options.length; i++) {
+  var newChar = getRandom(possChars);
+  result.push(newChar);
+ console.log("RESULT1",result); 
 
+  
+}
+for (let i = 0; i < guarChars.length; i++) {
+ result[i] = guarChars[i];
+ console.log("RESULT2",result);
+
+  
+}
+return result.join("");
+};
+
+// user options
+function getPasswordOptions() {
+  var length = parseInt(prompt("How many characters in your password? Enter a number between 1 and 28"));
+  console.log(length);
+  if (isNaN(length) === true) {
+    alert("Password length must be a number!");
+    return;
   };
 
-
-  // replicate to other option chars
+  if (length < 1 || length > 28) {
+    alert("Password must be betweeen 1 and 28 characters!");
+    return;
+  };
+// confirm boxes for character options
+  var hasSpecChars = confirm("Would you like to use symbols?");
+  var hasNum = confirm("Would you like to use numbers?");
+  var hasUpper = confirm("Would you like to use uppercase letters?");
+  var hasLower = confirm("Would you like to use lowercase letters?");
+console.log(confirm);
+  if (
+    hasSpecChars === false &&
+    hasNum === false &&
+    hasUpper === false &&
+    hasLower === false
+  ) {
+    alert("Password must contain at least 1 character type!");
+    return;
+  }
+  var passwordOptions = {
+    length,
+    hasSpecChars,
+    hasNum,
+    hasUpper,
+    hasLower,
+  };
+  return passwordOptions;
 
 
 };
 
 
-
-
-
-  // user prommpts and confirms
-  // adding prompt with alerts and hard stops 
-  function getPasswordOptions() {
-    var length = parseInt(prompt("How many characters in your password? Enter a number between 1 and 28"));
-    // if user doesn't enter a number
-    if (isNaN(length) === true) {
-      alert("Password length must be a number!");
-      return;
-    };
-    /// hard stop if user doesn't enter a number not >0<29 
-    if (length < 1 || length > 28) {
-      alert("Password must be betweeen 1 and 28 characters!");
-      return;
-    };
-    // confirm boxes for character options
-    var hasSpecChars = confirm("Would you like to use symbols?");
-    var hasNum = confirm("Would you like to use numbers?");
-    var hasUpper = confirm("Would you like to use uppercase letters?");
-    var hasLower = confirm("Would you like to use lowercase letters?");
-    // console.log(confirm);
-
-    if (
-      hasSpecChars === false &&
-      hasNum === false &&
-      hasUpper === false &&
-      hasLower === false
-    ) {
-      // if  user selects false (cancel) for all data types
-      alert("Password must contain at least 1 character type!");
-      return;
-    }
-    // all data choices
-    var passwordOptions = {
-      length,
-      hasSpecChars,
-      hasNum,
-      hasUpper,
-      hasLower,
-    };
-
-    return passwordOptions;
-
-
-  };
-
-
-
-
-
-
-
-
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
